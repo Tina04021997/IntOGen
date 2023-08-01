37f1850095c6aae7509ed68696c7588968f3628d
TinaYang <60873217+Tina04021997@users.noreply.github.com>
2023-08-01 15:37:03 -0700

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,3 @@
+nextflow run /home/ubuntu/intogen-plus/intogen.nf -resume -profile local --input /home/ubuntu/intogen-plus/INPUTS/DCIS_new/ --output /home/ubuntu/run_files/outputs/DCIS_new
+
+sudo shutdown -h now #this line will automatically shut down the AWS instance whoa!
