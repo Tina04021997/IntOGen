e3fe9947c53c88543e3d2cdf8b90d465b1d5ed4f
Ting (Tina) Yang <60873217+Tina04021997@users.noreply.github.com>
2024-12-28 23:37:19 -0800

    Update IntOGen.py

--- IntOGen.py
+++ IntOGen.py
@@ -1,56 +1,88 @@
-# %%
 import pandas as pd
 import os
 import glob
 
-# %%
-# Get a list of all file names in the folder
 # Folder path containing the files
-folder_path = '/Users/tingyang/Desktop/IntOGen_OL/LK'
+folder_path = '/tscc/lustre/restricted/alexandrov-ddn/users/tiy002/projects/OC_OL/NIH_AHEAD/Second_1123/ANALYSIS/final_vcfs'
 
 # Get a list of all file names in the folder
 file_names = [os.path.basename(file_path) for file_path in glob.glob(folder_path + '/*')]
 
-# %%
-# Function to transform the vcf dataframe
 def transform_values(row):
-    if len(row[2]) == len(row[3]): # SNP
-        return [row[0], row[1], row[1], "SNP", row[2], row[2], row[3]]
-    elif len(row[2]) > len(row[3]): # DEL
-        return [row[0], row[1]+ 1, row[1] + len(row[2]) -1, "DEL", row[2][1:len(row[2])], row[2][1:len(row[2])], "-"]
+    """Transform VCF values into IntOGen format"""
+    if len(row['REF']) == len(row['ALT']): # SNP
+        return pd.Series({
+            'Chromosome': row['CHROM'],
+            'Start_position': row['POS'],
+            'End_position': row['POS'],
+            'Variant_Type': 'SNP',
+            'Reference_Allele': row['REF'],
+            'Tumor_Seq_Allele1': row['REF'],
+            'Tumor_Seq_Allele2': row['ALT']
+        })
+    elif len(row['REF']) > len(row['ALT']): # DEL
+        return pd.Series({
+            'Chromosome': row['CHROM'],
+            'Start_position': row['POS'] + 1,
+            'End_position': row['POS'] + len(row['REF']) - 1,
+            'Variant_Type': 'DEL',
+            'Reference_Allele': row['REF'][1:],
+            'Tumor_Seq_Allele1': row['REF'][1:],
+            'Tumor_Seq_Allele2': '-'
+        })
     else: # INS
-        return [row[0], row[1], row[1] + 1, "INS", "-", row[3][1:len(row[3])], row[3][1:len(row[3])]]
+        return pd.Series({
+            'Chromosome': row['CHROM'],
+            'Start_position': row['POS'],
+            'End_position': row['POS'] + 1,
+            'Variant_Type': 'INS',
+            'Reference_Allele': '-',
+            'Tumor_Seq_Allele1': row['ALT'][1:],
+            'Tumor_Seq_Allele2': row['ALT'][1:]
+        })
 
-# %%
 def IntOGen_input(file):
-    header = ['Chromosome', 'Start_position', 'End_position', 'Variant_Type', 'Reference_Allele',
-          'Tumor_Seq_Allele1', 'Tumor_Seq_Allele2', 'Tumor_Sample_Barcode']
-    # Read desired columns from vcf files
-    df = pd.read_csv(folder_path + '/' + file, sep='\t', header=None, usecols=[0, 1, 3, 4])
-    # Remove "chr" in Chromosome column
-    df[0] = df[0].str.replace('chr', '')
-    # Renidex 
-    df = df.set_axis(range(len(df.columns)), axis=1)
-    # Creating desired IntOGen info
-    df = df.apply(transform_values, axis=1, result_type='expand')
-    # Add a new column and remove "_final.vcf"
-    df[7] = df.apply(lambda row: file[:-10], axis=1)
-    # Add the new headers to the dataframe
-    df.columns = header  
-    return df
-
+    """Process a single VCF file into IntOGen format"""
+    # Define column names for VCF
+    vcf_columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT']
     
+    # Read VCF file, skipping header lines that start with '#'
+    df = pd.read_csv(
+        os.path.join(folder_path, file), 
+        sep='\t',
+        comment='#',
+        names=vcf_columns,
+        usecols=['CHROM', 'POS', 'REF', 'ALT']
+    )
+    
+    # Remove "chr" prefix from chromosome column
+    df['CHROM'] = df['CHROM'].str.replace('chr', '')
+    
+    # Transform the data
+    result = df.apply(transform_values, axis=1)
+    
+    # Add sample name (filename without '.vcf')
+    result['Tumor_Sample_Barcode'] = file[:-4]
+    
+    return result
 
-# %%
+# Process all files
 dataframes = []
 for file in file_names:
-    matrix = IntOGen_input(file)
-    matrix
-    dataframes.append(matrix)
-# Concatenate the dataframes vertically with only the first dataframe's header
-result_df = pd.concat(dataframes, axis=0, ignore_index=True)
+    try:
+        matrix = IntOGen_input(file)
+        dataframes.append(matrix)
+        print(f"Processed {file} successfully")
+    except Exception as e:
+        print(f"Error processing {file}: {str(e)}")
 
-# Save the dataframe as a tab-delimited text file with headers
-result_df.to_csv(os.path.join(folder_path, 'data_mutations_extended.txt'), sep='\t', index=False)
-
-
+# Concatenate all dataframes
+if dataframes:
+    result_df = pd.concat(dataframes, axis=0, ignore_index=True)
+    
+    # Save the result
+    output_file = os.path.join(folder_path, 'data_mutations_extended.txt')
+    result_df.to_csv(output_file, sep='\t', index=False)
+    print(f"\nResults saved to: {output_file}")
+else:
+    print("No data was processed successfully")
